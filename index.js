07f268fea75b5ee7e3a3faf80f4ecd49c0b91f4c
EvilSourcerer <evilsourcerer@gmail.com>
2019-05-05 00:00:09 -0400

    fixed

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 console.log("MC ping started");
-var tcpping = require("tcp-ping");
+var mcping = require('mc-ping-updated');
 var colors = require('colors');
 const fs = require('fs');
 var finished = true;
 setInterval(function () {
     randomIP = randomNum(0, 256) + "." + randomNum(0, 256) + "." + randomNum(0, 256) + "." + randomNum(0, 256);
-    tcpping.probe(randomIP, 25565, function (err, available) {
-        console.log(randomIP + " is " + (available ? colors.green("online") : colors.red("offline")));
-        finished = true;
-        if (available) {
+    mcping(randomIP, 25565, function(err, res) {
+        if (err) {
+            console.log(randomIP + " is " + colors.red("offline"));
+        }
+        else {
+            console.log(randomIP + " is " + colors.green("online"));
             fs.appendFile('ips.txt', '\n' + randomIP, function (err) {
                 if(err) {
                     console.log("error saving to file! Permission problem?".red);
                 }
             });
         }
-    });
+    }, 3000);
 }, 200);
 
 function randomNum(min, max) {
